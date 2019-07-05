ac0c77f868b7c7fd70d82a10b564df84950514bc
Hsuan Wang <n10105981@qut.edu.au>
2019-07-05 12:37:36 +1000

    build component of weather form and titles setting openweather api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '0a2e31671a8a7afa15bfe84802130cff';
+
+class App extends React.Component{
+  getWeather = async (e)=>{
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render(){
+    return (
+      <div>
+        <Titles/>
+        <Form getWeather={this.getWeather}/>
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
